011a81a073bdfa629337f540dc19cd1b5ebd6db6
jkulton <jonkulton@gmail.com>
2021-05-05 21:36:57 -0400

    add constants for auth feature

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export default {
+  PSK_AUTH_HEADER_KEY: 'X-Ditto-PSK',
   PUT_CONTENT_TYPE: 'application/json',
   RESPONSE_HEADERS: {
     'content-type': 'application/json;charset=UTF-8',
@@ -6,6 +7,7 @@ export default {
   STATUS: {
     OK: 200,
     BAD_REQUEST: 400,
+    FORBIDDEN: 403,
     NOT_FOUND: 404,
     UNSUPPORTED_MEDIA_TYPE: 415,
     INTERNAL_SERVER_ERROR: 500,
