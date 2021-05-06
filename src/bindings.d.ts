76bfe124af8f6cb01c10399465dfed7276197f63
Jon Kulton <jonkulton@gmail.com>
2021-05-05 21:50:02 -0400

    Support Auth (#4)
    * add constants for auth feature
    * add DITTO_PSK global binding
    * add README docs for enabling/disabling auth
    * enforce auth if DITTO_PSK secret defined

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -2,4 +2,5 @@ export {};
 
 declare global {
   const DITTO_KV: KVNamespace
+  const DITTO_PSK: string
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { handleError, getRequestContext } from './utility';
 import constants from './constants';
 import actions from './actions';
 
-const { RESPONSE_HEADERS, STATUS } = constants;
+const { RESPONSE_HEADERS, PSK_AUTH_HEADER_KEY, STATUS } = constants;
 
 addEventListener('fetch', (event) => {
   const response = handleRequest(event.request).catch(handleError);
@@ -12,6 +12,17 @@ addEventListener('fetch', (event) => {
 async function handleRequest(request: Request): Promise<Response> {
   const requestContext = await getRequestContext(request, DITTO_KV);
   const action = actions[requestContext.method];
+
+  // If auth is enabled check auth before continuing
+  if (typeof DITTO_PSK !== 'undefined') {
+    const providedAuth = request.headers.get(PSK_AUTH_HEADER_KEY);
+    if (DITTO_PSK !== providedAuth) {
+      return new Response("Invalid or missing authorization.", {
+        headers: RESPONSE_HEADERS,
+        status: STATUS.FORBIDDEN
+      });
+    }
+  }
 
   // If HTTP request method doesn't match a supported action return error
   if (!action) {
